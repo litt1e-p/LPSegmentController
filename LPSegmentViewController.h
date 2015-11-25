1c3caa4ed94ee2a9687bb2c189a577a528aa3e32
litt1e-p <free9fw@gmail.com>
2015-11-26 00:05:27 +0800

    add files

---
+++ LPSegmentViewController.h
@@ -0,0 +1,15 @@
+//
+//  LPSegmentViewController.h
+//
+//  Created by litt1e-p on 15/11/25.
+//  Copyright © 2015年 litt1e-p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPSegmentViewController : UIViewController
+
+@property (nonatomic, strong) UIColor *segmentTinColor;
+- (void)loadViewControllers :(NSArray *)arrVc andSegementTitles :(NSArray *)arrTitle;
+
+@end
